4b3bb842163ba8752b0a6913eb35dd7aea01a040
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-24 08:55:40 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,30 +1,74 @@
 // import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+    const people = ['Rohim', 'Korim', 'Johir', 'Khalek', 'Jubair', 'Jakir'];
     const products = [
         { name: 'PhotoShop', price: '$90.50' },
         { name: 'Illustrator', price: '$80.99' },
         { name: 'PremiumPro', price: '$100.99' },
         { name: 'LightRoom', price: '$40.99' }
-
     ]
     return (
         <div className="App">
             <header>
-                {/* <Product name={products[0].name} price={products[0].price}></Product>
-                <Product name={products[1].name} price={products[1].price}></Product>
-                <Product name={products[2].name} price={products[2].price}></Product>
-                <Product name={products[3].name} price={products[3].price}></Product> */}
-
+                <Counter></Counter>
+                <Users></Users>
+                <ul>
+                    {
+                        people.map(people => <li>{people}</li>)
+                    }
+                    {
+                        products.map(products => <li>{products.name}</li>)
+                    }
+                </ul>
                 {/* dynamic call */}
-                <Product product={products[0]}></Product>
+                {/* <Product product={products[0]}></Product>
                 <Product product={products[1]}></Product>
                 <Product product={products[2]}></Product>
-                <Product product={products[3]}></Product>
+                <Product product={products[3]}></Product> */}
+
+                {/* Dynamic short version */}
+                {
+                    products.map(pd => <Product product={pd}> </Product>)
+                }
             </header>
         </div>
     );
+}
+function Users() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+    return (
+        <div>
+            <h3>User: {users.length}</h3>
+            <ul>
+                {
+                    users.map(user=><li>{user.name}</li>)
+                }
+            </ul>
+        </div>
+    )
+}
+function Counter() {
+    const [count, setCount] = useState(313);
+    const increaseHandle = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+    }
+    const decreaseHandle = () => setCount(count - 1)
+    return (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={increaseHandle}>Increase</button>
+            <button onClick={decreaseHandle}>Decrease</button>
+        </div >
+    )
 }
 function Product(props) {
     const productsStyle = {
